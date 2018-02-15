c3f13f5ee718e57f13182a773ceffb7a7f5324ef
danyal.saleem <danyal.saleem@graffitecs.com>
2018-02-15 17:15:51 +0500

    initial commits

---
+++ wp-content/themes/startuply/assets/javascripts/startuply-contentsliderfe9d.js
@@ -0,0 +1,2 @@
+!function(t){t.each(t(".vsc_content_slider"),function(){var a={pager:"true"===t(this).attr("data-pagination"),controls:"true"===t(this).attr("data-arrows"),auto:"true"===t(this).attr("data-autoplay"),infiniteLoop:"true"===t(this).attr("data-loop"),speed:t(this).attr("data-speed"),pause:t(this).attr("data-interval"),autoDelay:t(this).attr("data-interval"),nextText:"",prevText:""};t(this).find(".bx-slider").bxSlider(a)})}(window.jQuery);
+//# sourceMappingURL=startuply-contentslider.js.map
---
+++ wp-content/themes/startuply/assets/javascripts/startuply-parallaxfe9d.js
@@ -0,0 +1,2 @@
+jQuery(window).load(function(){jQuery(".parallax").each(function(){!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)&&jQuery(window).width>=1024&&$(this).parallax("50%",.4,!1)})});
+//# sourceMappingURL=startuply-parallax.js.map
